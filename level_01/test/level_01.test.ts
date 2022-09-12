510671e9ddb6623bfea694917ca2d6d64d28ea68
nari1021 <y.shhh000@gmail.com>
2022-09-12 22:22:16 +0900

    add: init folder

---
+++ level_01/test/level_01.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Level01 from '../lib/level_01-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/level_01-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Level01.Level01Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
